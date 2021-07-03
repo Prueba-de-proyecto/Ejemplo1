b5f97a6b20e4f6a12279cb173383039a09e66fed
Adria <Adria@Adrin>
2021-07-03 16:58:05 -0600

    cambio 1

--- src/Primero.java
+++ src/Primero.java
@@ -14,7 +14,7 @@ public class Primero {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    int hola=0;   // TODO code application logic here
     }
     
 }
